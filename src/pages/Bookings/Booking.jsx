f8fb1635408ba23c2348a0e12928ea639229ac42
kibriarobin97 <kibriarobin97@gmail.com>
2024-05-02 19:55:01 +0600

    added deleted functionality

--- src/pages/Bookings/Booking.jsx
+++ src/pages/Bookings/Booking.jsx
@@ -1,11 +1,16 @@
 
 
-const Booking = ({booking}) => {
+const Booking = ({ booking, handleDelete }) => {
 
-    const {img, service, price, date, name} = booking;
+    const {_id, img, service, price, date, name } = booking;
 
     return (
         <tr>
+            <td>
+                <button onClick={() => handleDelete(_id)} className="btn btn-circle hover:bg-primary hover:text-white btn-sm">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
+            </td>
             <td>
                 <div className="flex items-center gap-3">
                     <div className="avatar">
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -18,23 +18,43 @@ const Bookings = () => {
             })
     }, [])
 
+    const handleDelete = id => {
+        const proceed = confirm('Are you sure want to delete?')
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deleteCount > 0) {
+                        alert('Successfully deleted')
+                    }
+                    const remaining = bookings.filter(booked => booked._id !== id)
+                    setBookings(remaining)
+                })
+        }
+    }
+
     return (
         <div className="overflow-x-auto">
             <table className="table">
                 {/* head */}
                 <thead>
                     <tr>
+                        <th></th>
                         <th>Service</th>
                         <th>Price</th>
                         <th>Date</th>
-                        <th></th>
+                        <th>Status</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        bookings?.map(booking => <Booking 
-                        key={booking._id}
-                        booking={booking}
+                        bookings?.map(booking => <Booking
+                            key={booking._id}
+                            booking={booking}
+                            handleDelete={handleDelete}
                         ></Booking>)
                     }
                 </tbody>
